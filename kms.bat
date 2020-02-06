02ef16674b6cec9ecf85f2eb4e23c451f0987804
呓喵酱 <i@iotcat.me>
2020-02-06 18:39:25 +0000

    Update kms.bat

--- kms.bat
+++ kms.bat
@@ -1,6 +1,11 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 @echo off
 cd /d "%SystemRoot%\system32"
+echo. Clear old kms config...
+slmgr /upk
+slmgr /ckms
+slmgr /rearm
+echo. Setup new kms service...
 slmgr /skms kms.yimian.xyz
 slmgr /ato
 slmgr /xpr
